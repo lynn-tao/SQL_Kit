30eae988db3eecaba80fcb2260c62635002a1a95
Lynn Tao <lynntao@Lynns-MBP.fios-router.home>
2023-08-12 17:54:22 -0400

    pushing SQL_Kit to repo

---
+++ select_fulldata.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd
+
+connection = sqlite3.connect('database.db')
+cur = connection.cursor()
+
+# cur.execute(".header on")
+# cur.execute(".mode column")
+# cur.execute("UPDATE FullKitData SET Student='Baby' WHERE school='Providence Elementary';")
+data = cur.execute("SELECT Student, elementary_kits || ', ' || middle_kits AS Kits FROM FullKitData WHERE school='Providence Elementary';")
+for column in data:
+    print(column)
+# print(data)
+
+# PRINT FORMATTED DATA TABLE
+# print(pd.read_sql_query("SELECT * FROM FullKitData", connection))
+
+connection.close()
