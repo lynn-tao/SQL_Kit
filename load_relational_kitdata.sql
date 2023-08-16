890c28dd415414738092366f7aba116fb1f4825e
Lynn Tao <lynntao@Lynns-MBP.fios-router.home>
2023-08-16 16:28:19 -0400

    pushing new SQL scripts to repo

--- load_relational_kitdata.sql
+++ load_relational_kitdata.sql
@@ -70,41 +70,41 @@ CREATE TABLE assembly (
 );
 
 
-INSERT OR IGNORE INTO student(id,name,email,parent_name,parent_email,address)
+INSERT INTO student(id,name,email,parent_name,parent_email,address)
 VALUES
 (1, 'Ava', 'ava@gmail.com', 'Samantha', 'samantha@gmail.com', '1111 New Court Lane'),
 (2, 'Bob', 'bob@gmail.com', 'Javier', 'javier@gmail.com', '2222 New Court Lane'),
 (3, 'Clyde', 'clyde@gmail.com', 'Rachel', 'rachel@gmail.com', '3333 New Court Lane');
 
-INSERT OR IGNORE INTO school(id,name,address,type,Title_I,principal)
+INSERT INTO school(id,name,address,type,Title_I,principal)
 VALUES
 (1, 'Navy', '1111 New Court Lane', 'E', 0, 'John'),
 (2, 'Rocky Run', '1234 New Court Lane', 'M', 1, 'Sarah');
 
-INSERT OR IGNORE INTO study(student_id,school_id)
+INSERT INTO study(student_id,school_id)
 VALUES
-(1, 2),
+(1, 1),
 (2, 1),
 (3, 2);
 
-INSERT OR IGNORE INTO kit(id,type,name)
+INSERT INTO kit(id,type,name)
 VALUES
 (1, 'ES', 'Create an air cannon'),
 (2, 'MS', 'Build a volcano');
 
-INSERT OR IGNORE INTO purchase(student_id,kit_id, amount)
+INSERT INTO purchase(student_id,kit_id, amount)
 VALUES
 (1, 2, 5),
 (2, 1, 3),
 (3, 2, 4);
 
-INSERT OR IGNORE INTO part(id,name,price,manufacturer)
+INSERT INTO part(id,name,price,manufacturer)
 VALUES
 (1, 'Styrofoam', 10, 'Amazon'),
 (2, 'Cardboard Box', 15, 'Home Depot'),
 (3, 'Play-Doh', 5, 'Toys-R-Us');
 
-INSERT OR IGNORE INTO assembly(kit_id,part_id,amount)
+INSERT INTO assembly(kit_id,part_id,amount)
 VALUES
 (1, 1, 2),
 (1, 2, 3),
